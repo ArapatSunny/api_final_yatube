ca3f01874612b5c0f6f3a89f0ab2f967d6657325
ArapatSunny <fresco2209@gmail.com>
2022-10-09 22:22:22 +0300

    Hide Secret Key

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -1,9 +1,13 @@
 import os
+import environ
 from datetime import timedelta
+
+env = environ.Env()
+environ.Env.read_env()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'hhz7l-ltdismtf@bzyz+rple7*s*w$jak%whj@(@u0eok^f9k4'
+SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = True
 
